40b631b0071ee992d791a51b5e4fdf8f396084fa
Douglas Xavier Viégas <douglasviegas2011@hotmail.com>
2024-05-25 14:08:17 -0300

    Inicio do Projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Header } from "./components/Header";
+import "./styles/index.scss";
+
+function App() {
+  return (
+    <>
+      <main className="container">
+        <Header />
+      </main>
+    </>
+  );
+}
+
+export default App;
